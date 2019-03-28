b5110577d74b033ccb48284695faf2ceb228ae13
Musgrove, Forrest E <forrest-musgrove@uiowa.edu>
2019-03-28 11:58:54 -0500

    Initial commit with image streaming from Cozmo and hand segmentation detection

---
+++ HandDetector/detect_multi_threaded.py
@@ -0,0 +1,190 @@
+from HandDetector.utils import detector_utils as detector_utils
+import cv2
+import tensorflow as tf
+import multiprocessing
+from threading import Thread
+from multiprocessing import Queue, Pool
+import time
+from HandDetector.utils.detector_utils import WebcamVideoStream
+import datetime
+import argparse
+import cozmo
+import numpy
+
+from HandDetector import image_grab
+
+frame_processed = 0
+score_thresh = 0.15
+
+
+# Create a worker thread that loads graph and
+# does detection on images in an input queue and puts it on an output queue
+
+
+def worker(input_queue, output_queue, cap_params, frame_processed):
+    print(">> loading frozen model for worker")
+    detection_graph, sess = detector_utils.load_inference_graph()
+    sess = tf.Session(graph=detection_graph)
+    while True:
+        # print("> ===== in worker loop, frame ", frame_processed)
+        frame = input_queue.get()
+        if frame is not None:
+            # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
+            # while scores contains the confidence for each of these boxes.
+            # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
+
+            boxes, scores = detector_utils.detect_objects(
+                frame, detection_graph, sess)
+            # draw bounding boxes
+            detector_utils.draw_box_on_image(
+                cap_params['num_hands_detect'], cap_params["score_thresh"],
+                scores, boxes, cap_params['im_width'], cap_params['im_height'],
+                frame)
+            # add frame annotated with bounding box to queue
+            output_queue.put(frame)
+            frame_processed += 1
+        else:
+            output_queue.put(frame)
+    sess.close()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-src',
+        '--source',
+        dest='video_source',
+        type=int,
+        default=0,
+        help='Device index of the camera.')
+    parser.add_argument(
+        '-nhands',
+        '--num_hands',
+        dest='num_hands',
+        type=int,
+        default=2,
+        help='Max number of hands to detect.')
+    parser.add_argument(
+        '-fps',
+        '--fps',
+        dest='fps',
+        type=int,
+        default=1,
+        help='Show FPS on detection/display visualization')
+    parser.add_argument(
+        '-wd',
+        '--width',
+        dest='width',
+        type=int,
+        default=320,
+        help='Width of the frames in the video stream.')
+    parser.add_argument(
+        '-ht',
+        '--height',
+        dest='height',
+        type=int,
+        default=240,
+        help='Height of the frames in the video stream.')
+    parser.add_argument(
+        '-ds',
+        '--display',
+        dest='display',
+        type=int,
+        default=1,
+        help='Display the detected images using OpenCV. This reduces FPS')
+    parser.add_argument(
+        '-num-w',
+        '--num-workers',
+        dest='num_workers',
+        type=int,
+        default=4,
+        help='Number of workers.')
+    parser.add_argument(
+        '-q-size',
+        '--queue-size',
+        dest='queue_size',
+        type=int,
+        default=5,
+        help='Size of the queue.')
+    args = parser.parse_args()
+
+    input_q = Queue(maxsize=args.queue_size)
+    output_q = Queue(maxsize=args.queue_size)
+
+    cap_params = {}
+    frame_processed = 0
+    cap_params['im_width'], cap_params['im_height'] = (320, 240)
+    cap_params['score_thresh'] = score_thresh
+
+    # max number of hands we want to detect/track
+    cap_params['num_hands_detect'] = args.num_hands
+
+    print(cap_params, args)
+
+    # spin up workers to parallelize detection.
+    pool = Pool(args.num_workers, worker,
+                (input_q, output_q, cap_params, frame_processed))
+
+    start_time = datetime.datetime.now()
+    num_frames = 0
+    fps = 0
+    index = 0
+
+    cv2.namedWindow('Multi-Threaded Detection', cv2.WINDOW_NORMAL)
+
+    cozmo_thread = Thread(target=image_grab.run_cozmo_photostream, daemon=True).start()
+
+    print('Waiting for image')
+    x = 0
+    while True:
+        # Wait for a new image to be available from Cozmo
+        # wait_for_image = image_grab.photo_stream.image_available.wait()
+
+        # Grab the image and clear the event
+        frame = image_grab.photo_stream.latest_image
+        # image_grab.photo_stream.image_available.clear()
+
+        if frame is not None:
+            # Convert the frame from a PIL image to an OpenCV image format
+            frame = numpy.array(frame)
+
+            # Convert from RGB to BGR
+            frame = frame[:, :, ::-1].copy()
+
+            frame = cv2.flip(frame, 1)
+            index += 1
+
+            input_q.put(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            output_frame = output_q.get()
+
+            output_frame = cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR)
+
+            elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
+            num_frames += 1
+            fps = num_frames / elapsed_time
+            # print("frame ",  index, num_frames, elapsed_time, fps)
+
+            if output_frame is not None:
+                if args.display > 0:
+                    if args.fps > 0:
+                        detector_utils.draw_fps_on_image("FPS : " + str(int(fps)),
+                                                         output_frame)
+                    cv2.imshow('Multi-Threaded Detection', output_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
+                    if num_frames == 400:
+                        num_frames = 0
+                        start_time = datetime.datetime.now()
+                    else:
+                        print("frames processed: ", index, "elapsed time: ",
+                              elapsed_time, "fps: ", str(int(fps)))
+            else:
+                # print("video end")
+                break
+    elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
+    fps = num_frames / elapsed_time
+    print("fps", fps)
+    pool.terminate()
+    cv2.destroyAllWindows()
---
+++ HandDetector/image_grab.py
@@ -0,0 +1,62 @@
+import cozmo
+import asyncio
+
+from Common.colors import Colors
+
+from PIL import Image
+
+
+# region CozmoPhotoStream Class
+class CozmoPhotoStream:
+    def __init__(self):
+        self.robot: cozmo.robot.Robot = None
+        self.cubes = []
+        camera_image = Image.open('../res/FaceImages/camera.png').resize(cozmo.oled_face.dimensions(), Image.BICUBIC)
+        self.face_image = cozmo.oled_face.convert_image_to_screen_data(camera_image, invert_image=True)
+        self.latest_image: Image = None
+
+    async def on_camera_image(self, evt=None, **kwargs):
+        # Grab the latest frame and save the raw image as a greyscale png file
+        if self.robot.world.latest_image is not None:
+            self.latest_image: Image = self.robot.world.latest_image.raw_image.convert('RGB')
+
+    async def robot_say(self, text):
+        await self.robot.say_text(text, duration_scalar=0.6).wait_for_completed()
+
+    async def run(self):
+        # Turn backpack lights to RED
+        self.robot.set_all_backpack_lights(Colors.RED)
+
+        # Move lift out of the way of the camera
+        await self.robot.set_lift_height(0.0).wait_for_completed()
+
+        # Settings for signals from Cozmo's camera
+        self.robot.camera.image_stream_enabled = True
+        self.robot.camera.color_image_enabled = True
+
+        self.robot.set_all_backpack_lights(Colors.GREEN)
+
+        # Add event handler to process each image as it becomes available
+        self.robot.camera.add_event_handler(cozmo.robot.camera.EvtNewRawCameraImage, self.on_camera_image)
+
+        # self.robot.display_oled_face_image(self.face_image, 2000, in_parallel=True)
+
+        # await self.robot_say('Awaiting a command')
+
+        while True:
+            # Wait for SIG-INT and keep the thread controlling Cozmo alive
+            await asyncio.sleep(0.5)
+
+
+# endregion
+
+photo_stream = CozmoPhotoStream()
+
+
+async def cozmo_program(robot: cozmo.robot.Robot):
+    photo_stream.robot = robot
+    await photo_stream.run()
+
+
+def run_cozmo_photostream():
+    cozmo.run_program(cozmo_program, use_viewer=False)
