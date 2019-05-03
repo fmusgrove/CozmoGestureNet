fc3cd86715f6e987ff53542ab444554f36aadfbb
Musgrove, Forrest E <forrest-musgrove@uiowa.edu>
2019-05-03 04:30:20 -0500

    Cleaned up and refactored some things, switched detector utilities over to new ssd5-based model

--- HandDetector/utils/detector_utils.py
+++ HandDetector/utils/detector_utils.py
@@ -1,43 +1,39 @@
-# Utilities for object detector.
-
-import numpy as np
 import sys
-import tensorflow as tf
-import os
 from threading import Thread
+
 import cv2
+import numpy as np
+import tensorflow as tf
+
 from HandDetector.utils import label_map_util
 
 detection_graph = tf.Graph()
 sys.path.append("..")
 
-MODEL_NAME = 'HandDetector/hand_inference_graph'
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join(MODEL_NAME, 'hand_label_map.pbtxt')
+# Path to frozen detection graph used in object detection
+HAND_DETECT_CKPT = 'Models/hand_detect_graph/ssd5_optimized_inference_graph.pb'
+# List of the strings that is used to add correct label for each box
+DETECT_LABELS = 'Models/hand_detect_graph/hand_label_map.pbtxt'
 
 NUM_CLASSES = 1
-# load label map
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+# Load label map
+label_map = label_map_util.load_labelmap(DETECT_LABELS)
 categories = label_map_util.convert_label_map_to_categories(
     label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 
-# Load a frozen infrerence graph into memory
-def load_inference_graph():
-    # load frozen tensorflow model into memory
-    print("> ====== loading HAND frozen graph into memory")
+def load_detect_graph():
+    # Load frozen TensorFlow model into memory
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        with tf.gfile.GFile(HAND_DETECT_CKPT, 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(od_graph_def, name='')
         sess = tf.Session(graph=detection_graph)
-    print(">  ====== Hand Inference graph loaded.")
+    print('>  ====== Hand detection graph loaded.')
     return detection_graph, sess
 
 
